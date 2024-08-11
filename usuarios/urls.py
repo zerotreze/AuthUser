f61b7acc335c5fd462c1b3c4cc982eb8a0e54d89
José Vitor <blackdarkfur@hotmail.com>
2024-08-11 18:00:19 -0300

    Update

--- usuarios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cadastro/', views.cadastro, name="cadastro"), # type: ignore
-    path('logar/', views.logar, name="logar"), # type: ignore
-    
-]
